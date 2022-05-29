54a8c85216c3f7c7832a55ed9986d16098fbd70d
germanOwO <gatalyan@ucm.es>
2022-05-29 16:58:27 +0200

    remove unused code in panels

--- src/gui/LeftPanel2.java
+++ src/gui/LeftPanel2.java
@@ -1,24 +1,18 @@
 package gui;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import javax.swing.BoxLayout;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
-import javax.swing.JSpinner;
 import javax.swing.JTextField;
-import javax.swing.SpinnerNumberModel;
 
 import org.javatuples.Pair;
 
 import algorithm.functions.*;
-import algorithm.individuos.gen.GenRange;
 import algorithm.operations.Operation;
 import algorithm.population.Poblacion;
-import auxiliar.MyGui;
 
 public class LeftPanel2 implements View {
 
@@ -27,89 +21,23 @@ public class LeftPanel2 implements View {
     private JComboBox<Operation> cruceComboBox;
     private JComboBox<Function> functionComboBox;
 
-    private JPanel rangesPanelWrap = new JPanel();
-	private JPanel rangesPanel = new JPanel();
-	private JSpinner numGenesSpinner = new JSpinner(new SpinnerNumberModel(1, 1, 10, 1));
-	private List<Pair<JSpinner, JSpinner>> rangesSpinners = new ArrayList<Pair<JSpinner,JSpinner>>();
-
 	private JTextField mutacionTextField = new JTextField("50");
 	private JTextField cruceTextField = new JTextField("70");
 	private JTextField presionTextField = new JTextField("1.5");
-	private Integer numGenes = null;
 
     public LeftPanel2(JComboBox<Operation> seleccionComboBox, JComboBox<Operation> mutacionComboBox,
             JComboBox<Operation> cruceComboBox, JComboBox<Function> functionComboBox) {
-        functionComboBox.addActionListener(e -> numGenes = ((Function) functionComboBox.getSelectedItem()).getNumVars());
-		functionComboBox.addActionListener(e -> setRanges());
-		numGenesSpinner.addChangeListener(e -> updateNumGenes((int) numGenesSpinner.getValue()));
         this.seleccionComboBox = seleccionComboBox;
         this.cruceComboBox = cruceComboBox;
         this.mutacionComboBox = mutacionComboBox;
         this.functionComboBox = functionComboBox;
-
-		rangesPanelWrap.setLayout(new BoxLayout(rangesPanelWrap, BoxLayout.Y_AXIS));
-		rangesPanel.setLayout(new BoxLayout(rangesPanel, BoxLayout.Y_AXIS));
-    }
-
-	private void addRanges(int start, int end) {
-		JPanel horizontalPanel;
-		SpinnerNumberModel spinner1;
-		SpinnerNumberModel spinner2;
-		Function function = (Function) functionComboBox.getSelectedItem();
-		List<GenRange> ranges = function.getRanges();
-		for (int i = start; i < end; i++) {
-			int idx = i;
-			if (function.getNumVars() == null) idx = 0;
-			spinner1 = new SpinnerNumberModel(ranges.get(idx).getMin(), Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, 0.1);
-			spinner2 = new SpinnerNumberModel(ranges.get(idx).getMax(), Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, 0.1);
-			
-			horizontalPanel = new JPanel();
-			MyGui.addLabel("Gen " + (i + 1), horizontalPanel);
-			rangesSpinners.add(new Pair<JSpinner,JSpinner>(new JSpinner(spinner1), new JSpinner(spinner2)));
-			horizontalPanel.add(rangesSpinners.get(i).getValue0());
-			horizontalPanel.add(rangesSpinners.get(i).getValue1());
-			rangesPanel.add(horizontalPanel);
-		}
-	}
-
-	private void updateNumGenes(int n) {
-		
-		if (n - numGenes > 0) {
-			addRanges(numGenes, n);
-		} else for (int i = rangesSpinners.size() - 1; i >= n; i--) {
-			rangesSpinners.remove(rangesSpinners.size() - 1);
-			rangesPanel.remove(i);
-		} numGenes = n;
-	}
-    
-    private void setRanges() {
-
-		Function function = (Function) functionComboBox.getSelectedItem();
-		if (function.getRanges() == null) return;
-
-		rangesSpinners.clear();
-		rangesPanel.removeAll();
-		rangesPanelWrap.removeAll();
-		numGenesSpinner.setValue(1);
-
-		if (function.getNumVars() == null) {
-			MyGui.addLabel("Number of Genes", rangesPanelWrap);
-			rangesPanelWrap.add(numGenesSpinner);
-			numGenes = 1;
-		} else numGenes = function.getNumVars();
-
-		addRanges(0, numGenes);
-
-		rangesPanelWrap.add(rangesPanel);
     }
 
     @Override
     public List<Pair<List<Pair<String, JComponent>>, JPanel>> getPanelList() {
-        setRanges();
         return Arrays.asList(
             new Pair<>(Arrays.asList(
-				new Pair<>("Funcion", functionComboBox),
-				new Pair<>("", rangesPanelWrap)
+				new Pair<>("Funcion", functionComboBox)
 			), null),
             new Pair<>(Arrays.asList(
 				new Pair<>("Selecciones", seleccionComboBox),
@@ -130,6 +58,4 @@ public class LeftPanel2 implements View {
 	public Poblacion getPoblacion() {
 		return null;
 	}
-    
-
 }
